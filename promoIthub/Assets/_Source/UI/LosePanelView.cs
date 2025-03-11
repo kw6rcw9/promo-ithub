524d555abf8eb955cfd654084d58b542a8aeab07
Kate Dushina <120956700+kw6rcw9@users.noreply.github.com>
2025-03-12 00:11:20 +0300

    Исправила съезжающий экран проигрыша

--- promoIthub/Assets/_Source/UI/LosePanelView.cs
+++ promoIthub/Assets/_Source/UI/LosePanelView.cs
@@ -3,23 +3,48 @@ using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 using TimerSystem;
+using UnityEngine.UI;
 using Timer = TimerSystem.Timer;
 
 namespace UI
 {
     public class LosePanelView : MonoBehaviour
     {
-        [SerializeField] private GameObject panel;
+        [SerializeField] private RectTransform panel;
+        [SerializeField] private Image fadePanel;
         [SerializeField] private GameObject timer;
+        [SerializeField] private float duration = 0.5f;
+        [SerializeField] private float fadeDuration = 0.5f;
+        private Vector2 startPosition;
+        private Vector2 targetPosition;
+        [SerializeField] private Ease easeType = Ease.OutBounce; // Тип анимации
+
+        private void Start()
+        {
+            // Запоминаем стартовую позицию (за экраном)
+            startPosition = panel.anchoredPosition;
+            // Конечная позиция - центр экрана
+            targetPosition = new Vector2(startPosition.x, 0);
+
+            // Начинаем с панели вне экрана
+            panel.anchoredPosition = startPosition;
+        }
 
         public void ShowPanel()
         {
-            print("SHOW");
-           
-            panel.SetActive(true);
-           panel.transform.DOMove(new Vector3(0, panel.transform.position.y - 7.2f ,0), 1.2f);
+            FadeIn();
+            print("SHOW"); 
+            panel.gameObject.SetActive(true); 
+            panel.DOAnchorPos(targetPosition,duration).SetEase(easeType); 
             timer.SetActive(false);
             
         }
+        
+        private void FadeIn()
+        {
+            fadePanel.DOFade(0.5f, fadeDuration);
+        }
+        
+        
     }
 }
